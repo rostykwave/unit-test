d5a02d35e222a512c5a38e27cc4e72cd0cb1fec3
Rostyslav Kovalchuk <rostykwave@gmail.com>
2023-11-25 22:24:31 +0200

    part-5

--- src/order-total/order-total.test.js
+++ src/order-total/order-total.test.js
@@ -1,35 +1,46 @@
 const orderTotal = require("./order-total");
 
-const emptyFunction = () => {};
-
 it("calls vatapi.com correctly", () => {
-  let isFakeFetchCalled = false;
-  const fakeFetch = (url) => {
-    expect(url).toBe("https://vatapi.com/v1/country-code-check?code=DE");
-    isFakeFetchCalled = true;
+  const fakeProces = {
+    env: {
+      VAT_API_KEY: "key123",
+    },
   };
-  orderTotal(fakeFetch, {
+
+  const fakeFetch = (url, opts) => {
+    expect(opts.headers.apikey).toBe("key123");
+    expect(url).toBe("https://vatapi.com/v1/country-code-check?code=DE");
+    return Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          rates: {
+            standard: {
+              value: 19,
+            },
+          },
+        }),
+    });
+  };
+  return orderTotal(fakeFetch, fakeProces, {
     country: "DE",
     items: [{ name: "Dragon waffles", price: 20, quantity: 2 }],
   }).then((result) => {
-    expect(isFakeFetchCalled).toBe(true);
+    expect(result).toBe(20 * 2 * 1.19);
   });
 });
 
-it("if country code specified");
-
 it("Quantity", () =>
-  orderTotal(emptyFunction, {
+  orderTotal(null, null, {
     items: [{ name: "Dragon candy", price: 2, quantity: 3 }],
   }).then((result) => expect(result).toBe(6)));
 
 it("No quantity specified", () =>
-  orderTotal(emptyFunction, {
+  orderTotal(null, null, {
     items: [{ name: "Dragon candy", price: 3 }],
   }).then((result) => expect(result).toBe(3)));
 
 it("Happy path (Example 1)", () =>
-  orderTotal(emptyFunction, {
+  orderTotal(null, null, {
     items: [
       { name: "Dragon food", price: 8, quantity: 1 },
       { name: "Dragon cage (small)", price: 800, quantity: 1 },
@@ -37,7 +48,7 @@ it("Happy path (Example 1)", () =>
   }).then((result) => expect(result).toBe(808)));
 
 it("Happy path (Example 2)", () =>
-  orderTotal(emptyFunction, {
+  orderTotal(null, null, {
     items: [
       { name: "Dragon collar", price: 20, quantity: 1 },
       { name: "Dragon chew toy", price: 40, quantity: 1 },
